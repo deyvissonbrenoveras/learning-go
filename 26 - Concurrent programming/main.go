177288b03d8a9fd934dd59b5b0f71936859a369f
Deyvisson Breno <deyvison.meireles.ext@bayer.com>
2024-11-10 17:18:22 -0300

    go routine

--- 26 - Concurrent programming/main.go
+++ 26 - Concurrent programming/main.go
@@ -14,5 +14,22 @@ func main() {
 		wg.Done()
 	}()
 	fmt.Println("This is synchronous")
+	matrix(50000)
 	wg.Wait()
+}
+
+func matrix(routines int) {
+	var wg sync.WaitGroup
+	for i := 0; i < routines; i++ {
+		wg.Add(1)
+		go runAsync(10000, &wg)
+	}
+	wg.Wait()
+}
+
+func runAsync(v int, wg *sync.WaitGroup) {
+	for i := 0; i < v; i++ {
+		fmt.Printf("%d", i)
+	}
+	wg.Done()
 }
