3863c485d3547265e776671c938285ca0feff150
Deyvisson Breno <deyvissonbrenoveras@gmail.com>
2024-12-17 22:41:39 -0300

    inventory summary

--- Exercises/inventory management/inventory/inventory.go
+++ Exercises/inventory management/inventory/inventory.go
@@ -1,11 +1,40 @@
 package inventory
 
-import "inventory-management/products"
+import (
+	"fmt"
+	"inventory-management/products"
+)
 
-func AddProduct(product products.Product) {
+func AddProduct(product products.Product) error {
+	for _, prod := range storedProducts {
+		if prod.Id == product.Id {
+			return fmt.Errorf(`product with id %s already exists`, product.Id)
+		}
+	}
 	storedProducts = append(storedProducts, product)
+	return nil
+}
+
+func UpdateProduct(product products.Product) error {
+	for i, prod := range storedProducts {
+		if prod.Id == product.Id {
+			storedProducts[i] = product
+			return nil
+		}
+	}
+	return fmt.Errorf(`product with id %s not found`, product.Id)
 }
 
 func GetAllProducts() []products.Product {
 	return storedProducts
+}
+
+func GetInventorySummary() (totalValue float32, productCount int) {
+
+	for _, prod := range storedProducts {
+		totalValue += float32(prod.Price)
+		productCount += 1
+	}
+
+	return totalValue, productCount
 }
--- Exercises/inventory management/main.go
+++ Exercises/inventory management/main.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+var separator = strings.Repeat("-", 20)
+
 func main() {
 	fmt.Println("Inventory management system\n")
 loop:
@@ -16,8 +18,12 @@ loop:
 		switch selectedOption {
 		case 1:
 			addProduct()
+		case 2:
+			updateInventory()
 		case 3:
 			showInventory()
+		case 4:
+			getInventorySummary()
 		case 5:
 			menu.ClearConsole()
 			fmt.Println("Quitting Inventory Management System")
@@ -32,20 +38,45 @@ func addProduct() {
 		fmt.Println(err)
 		return
 	}
+	err = inventory.AddProduct(product)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	fmt.Printf("Product registered: %+v\n\n", product)
-	inventory.AddProduct(product)
+}
+
+func updateInventory() {
+	product, err := menu.GetProductInputFromUser()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = inventory.UpdateProduct(product)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Product updated: %+v\n\n", product)
 }
 
 func showInventory() {
 	products := inventory.GetAllProducts()
-	separator := strings.Repeat("-", 20)
 
 	for index, product := range products {
 		fmt.Println(separator)
 		fmt.Printf("Product %d\n", index+1)
-		fmt.Printf("Id: %s\n", product.Name)
+		fmt.Printf("Id: %s\n", product.Id)
 		fmt.Printf("Name: %s\n", product.Name)
 		fmt.Printf("Price: %.2f\n", product.Price)
 	}
+	fmt.Println(separator)
+}
+
+func getInventorySummary() {
+	totalValue, productCount := inventory.GetInventorySummary()
+	fmt.Println(separator)
+	fmt.Println("\nInventory summary: \n")
+	fmt.Printf("Product count: %d\nProducts total price: R$ %.2f\n\n", productCount, totalValue)
 	fmt.Println(separator)
 }
